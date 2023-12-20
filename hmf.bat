397ea95fd948bce4d520ddefdb33030126214244
hardlyz <44165579+hardlyz@users.noreply.github.com>
2023-12-20 19:16:34 +0700

    Add files via upload

---
+++ hmf.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+chcp 1251
+cls
+
+
+if "%~1"=="" (
+    echo You didn't drag and drop a file onto this file.
+    pause
+    exit /b
+)
+
+set "file=%~1"
+
+attrib +h +s "%file%"
+
+echo Attributes successfully assigned to file %file%.
+pause
